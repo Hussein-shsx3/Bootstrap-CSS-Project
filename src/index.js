ac7168fee137ef212c0a0f9e7731b74b5aa94a79
Hussein-shsx3 <husseinshsx3@gmail.com>
2024-10-15 21:49:20 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./Pages/Home";
+
+import { Provider } from "react-redux";
+import { storeApp } from "./Store";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={storeApp}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
+);
